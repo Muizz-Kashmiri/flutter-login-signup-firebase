e3c1b05a22911ac87aacd0bb5ed8aa6b7756c196
Muizz Elahi Butt <muizzbutt79@gmail.com>
2023-03-13 23:13:44 +0500

    all to all

---
+++ lib/ui/auth/onBoarding/data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+///list of strings containing onBoarding titles
+final List<String> titlesList = [
+  'Flag It',
+  'Firebase Auth',
+  'Facebook Login',
+  'Sentiment Analysis',
+  'Jump straight into the action',
+];
+
+/// list of strings containing onBoarding subtitles, the small text under the
+/// title
+final List<String> subtitlesList = [
+  'Flag negative comments using one API',
+  'Use Firebase for user managements',
+  'Leverage Facebook to log in user easily',
+  'Using Sentiment Analysis and Machine Learning',
+  'Get Started'
+];
+
+/// list containing image paths or IconData representing the image of each page
+
+final List<dynamic> imageList = [
+  Icons.developer_mode,
+  Icons.layers,
+  Icons.account_circle,
+  'assets/images/ic_launcher_round.png',
+  Icons.code
+];
